b4b29566cb49927578f738d0bcf8d217aef0009a
Rustam Aliev <azerich@yandex.ru>
2026-03-27 17:38:26 +0300

    GatewayService implementation

---
+++ infrastructure/gateway-service/src/main/java/com/aliev/mes/GatewayService.java
@@ -0,0 +1,15 @@
+package com.aliev.mes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayService.class, args);
+    }
+}
